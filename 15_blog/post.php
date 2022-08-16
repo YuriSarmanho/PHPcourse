1f3250caa5839d6e514e4288fcc738a3a327744a
YuriSarmanho <yuri.sarmanho@hotmail.com>
2022-08-16 16:18:18 -0300

    estilo da pagina de post finalizado

--- 15_blog/post.php
+++ 15_blog/post.php
@@ -30,21 +30,21 @@
             Possimus incidunt, veritatis sequi, tempore nisi culpa ipsum consequatur consequuntur officiis soluta aperiam? Voluptatibus quo officiis, saepe, illum nam nemo laudantium sunt fugit temporibus odit sapiente nulla alias ad aliquam?
             Mollitia eligendi assumenda incidunt itaque in, exercitationem quia voluptatibus saepe distinctio vero recusandae esse! Incidunt praesentium, ad pariatur soluta delectus est. Consectetur numquam id suscipit tempora doloremque atque veritatis esse.</p>        
         </div>
+        <aside id="nav-container">
+            <h3 id="tags-list">Tags</h3>
+            <ul id="tag-list">
+                <?php foreach($currentPost['tags'] as $tag): ?>
+                   <li><a href="#"><?=$tag?></a></li>
+                <?php endforeach; ?>   
+            </ul>
+            <h3 id='categorias-title'>Categorias</h3>
+            <ul id="categories-list">
+            <?php foreach($currentPost as $categories): ?>
+                   <li><a href="#"><?=$categories?></a></li>
+                <?php endforeach; ?> 
+            </ul>
+        </aside>
     </main>
-    <aside id="nav-container">
-        <h3 id="tags-list">Tags</h3>
-        <ul id="tag-list">
-            <?php foreach($currentPost['tags'] as $tag): ?>
-               <li><a href="#"><?=$tag?></a></li>
-            <?php endforeach; ?>   
-        </ul>
-        <h3 id='categorias-title'>Categorias</h3>
-        <ul id="categories-list">
-        <?php foreach($currentPost as $categories): ?>
-               <li><a href="#"><?=$categories?></a></li>
-            <?php endforeach; ?> 
-        </ul>
-    </aside>
 <?php
     include_once("templates/footer.php");
 ?>
